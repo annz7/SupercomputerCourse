53f9b3d9730c6c5f481132b7bb4f831043f36f1b
annz7 <annz7@bk.ru>
2022-04-19 15:23:24 +0700

    fix tabs

--- Task3/data_race_fix.cpp
+++ Task3/data_race_fix.cpp
@@ -2,18 +2,18 @@
 #include <iostream>
 using namespace std;
 int main(int argc, char **argv) {
-const int size = (argc > 1 ? atoi(argv[1]) : 1000);
-int *a = new int[size];
-#pragma omp parallel for
-for(int i = 0; i < size; i++) {
-a[i] = i;
-}
-int sum = 0;
-#pragma omp parallel for reduction(+:sum)
-for(int i = 0; i < size; i++) {
-sum += a[i];
-}
-cout << "Sum = " << sum << endl;
-delete[] a;
-return 0;
+	const int size = (argc > 1 ? atoi(argv[1]) : 1000);
+	int *a = new int[size];
+	#pragma omp parallel for
+	for(int i = 0; i < size; i++) {
+		a[i] = i;
+	}
+	int sum = 0;
+	#pragma omp parallel for reduction(+:sum)
+	for(int i = 0; i < size; i++) {
+		sum += a[i];
+	}
+	cout << "Sum = " << sum << endl;
+	delete[] a;
+	return 0;
 }
